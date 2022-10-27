d36341477df64091e1248abbfa962cee1373e763
Joseph Archer <viewtiful.kja@gmail.com>
2022-10-26 21:37:06 -0400

    changes to port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const htmlRoutes = require("./routes/htmlRoutes")
 const noteRoutes = require("./routes/noteRoutes")
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 // Create instance of express
 const app = express();
